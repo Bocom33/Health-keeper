875dcf8614e32668b59039dee0d28be96bc9d6ba
Carolinefourcade <caroannab33@gmail.com>
2020-06-25 11:08:47 +0200

    form medical record

--- src/Controller/MedicalRecordController.php
+++ src/Controller/MedicalRecordController.php
@@ -3,17 +3,51 @@
 
 namespace App\Controller;
 
-
+use App\Entity\MedicalRecord;
+use App\Repository\AnnoyanceRepository;
+use App\Entity\Annoyance;
+use App\Form\AnnoyanceType;
+use App\Repository\AnnoyanceZoneRepository;
+use App\Entity\AnnoyanceZone;
+use App\Repository\PainIntensityRepository;
+use App\Entity\PainIntensityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @Route("/medical", name="medical")
- * 
+ *
  */
 class MedicalRecordController extends AbstractController
 {
-    public function index (AnnoyanceRepository $annoyanceRepository ) {
-        
+
+    /**
+     * @Route("/", name="_record")
+     * @param Request $request
+     * @param AnnoyanceRepository $annoyanceRepository
+     * @param AnnoyanceZoneRepository $annoyanceZoneRepository
+     * @param PainIntensityRepository $painIntensityRepository
+     * @return Response
+     */
+
+    public function index(
+        Request $request,
+        AnnoyanceRepository $annoyanceRepository,
+        AnnoyanceZoneRepository $annoyanceZoneRepository,
+        PainIntensityRepository $painIntensityRepository
+    ): Response {
+
+        $medicalRecord = new MedicalRecord;
+        $formAnnoyance = $this->createForm(AnnoyanceType::class, $medicalRecord);
+        $formAnnoyance->handleRequest($request);
+
+        return $this->render('medical/index.html.twig', [
+            'form_annoyance' => $formAnnoyance->createView(),
+            'annoyances' => $annoyanceRepository->findAll(),
+            'annoyancesZones' => $annoyanceZoneRepository->findAll(),
+            'painsIntensities' => $painIntensityRepository->findAll()
+
+        ]);
     }
 }
--- src/Form/AnnoyanceType.php
+++ src/Form/AnnoyanceType.php
@@ -3,7 +3,9 @@
 namespace App\Form;
 
 use App\Entity\Annoyance;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,14 +14,15 @@ class AnnoyanceType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-        ;
+            ->add('name', TextType::class, [
+                'label' => 'Désagréments'
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Annoyance::class,
+            'data_class' => null,
         ]);
     }
 }
